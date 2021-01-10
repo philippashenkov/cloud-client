29ebb3cf591a1657b9427b3442266fb401a000fe
Philip <reikoslv@gmail.com>
2021-01-10 19:34:15 +0200

    add search, sort, styled filelist and avatar logic

--- src/actions/file.js
+++ src/actions/file.js
@@ -1,16 +1,31 @@
 import axios from 'axios'
 import {addFile, deleteFileAction, setFiles} from "../reducers/fileReducer";
 import {addUploadFile, changeUploadFile, showUploader} from "../reducers/uploadReducer";
+import {hideLoader, showLoader} from "../reducers/appReducer";
+import {API_URL} from "../config";
 
-export function getFiles(dirId) {
+export function getFiles(dirId, sort) {
     return async dispatch => {
         try {
-            const response = await axios.get(`http://localhost:5000/api/files${dirId ? '?parent='+dirId : ''}`, {
+            dispatch(showLoader())
+            let url = `${API_URL}api/files`
+            if (dirId) {
+                url = `${API_URL}api/files?parent=${dirId}`
+            }
+            if (sort) {
+                url = `${API_URL}api/files?sort=${sort}`
+            }
+            if (dirId && sort) {
+                url = `${API_URL}api/files?parent=${dirId}&sort=${sort}`
+            }
+            const response = await axios.get(url, {
                 headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
-            })
+            });
             dispatch(setFiles(response.data))
         } catch (e) {
             alert(e.response.data.message)
+        } finally {
+            dispatch(hideLoader())
         }
     }
 }
@@ -18,7 +33,7 @@ export function getFiles(dirId) {
 export function createDir(dirId, name) {
     return async dispatch => {
         try {
-            const response = await axios.post(`http://localhost:5000/api/files`,{
+            const response = await axios.post(`${API_URL}api/files`,{
                 name,
                 parent: dirId,
                 type: 'dir'
@@ -43,7 +58,7 @@ export function uploadFile(file, dirId) {
             const uploadFile = {name: file.name, progress: 0, id: Date.now()}
             dispatch(showUploader())
             dispatch(addUploadFile(uploadFile))
-            const response = await axios.post(`http://localhost:5000/api/files/upload`, formData, {
+            const response = await axios.post(`${API_URL}api/files/upload`, formData, {
                 headers: {Authorization: `Bearer ${localStorage.getItem('token')}`},
                 onUploadProgress: progressEvent => {
                     const totalLength = progressEvent.lengthComputable ? progressEvent.total : progressEvent.target.getResponseHeader('content-length') || progressEvent.target.getResponseHeader('x-decompressed-content-length');
@@ -62,7 +77,7 @@ export function uploadFile(file, dirId) {
 
 
 export async function downloadFile(file) {
-    const response = await fetch(`http://localhost:5000/api/files/download?id=${file._id}`,{
+    const response = await fetch(`${API_URL}api/files/download?id=${file._id}`,{
         headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`
         }
@@ -82,7 +97,7 @@ export async function downloadFile(file) {
 export function deleteFile(file) {
     return async dispatch => {
         try {
-            const response = await axios.delete(`http://localhost:5000/api/files?id=${file._id}`,{
+            const response = await axios.delete(`${API_URL}api/files?id=${file._id}`,{
                 headers:{
                     Authorization: `Bearer ${localStorage.getItem('token')}`
                 }
@@ -91,6 +106,23 @@ export function deleteFile(file) {
             alert(response.data.message)
         } catch (e) {
             alert(e?.response?.data?.message)
+        }
+    }
+}
+
+export function searchFiles(search) {
+    return async dispatch => {
+        try {
+            const response = await axios.get(`${API_URL}api/files/search?search=${search}`,{
+                headers:{
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+            dispatch(setFiles(response.data))
+        } catch (e) {
+            alert(e?.response?.data?.message)
+        } finally {
+            dispatch(hideLoader())
         }
     }
 }
--- src/components/disk/Disk.jsx
+++ src/components/disk/Disk.jsx
@@ -4,18 +4,20 @@ import {createDir, getFiles, uploadFile} from "../../actions/file";
 import FileList from "./fileList/FileList";
 import './disk.css'
 import Popup from "./Popup";
-import {setCurrentDir, setPopupDisplay} from "../../reducers/fileReducer";
+import {setCurrentDir, setFileView, setPopupDisplay} from "../../reducers/fileReducer";
 import Uploader from "./uploader/Uploader";
 
 const Disk = () => {
     const dispatch = useDispatch()
     const currentDir = useSelector(state => state.files.currentDir)
+    const loader = useSelector(state => state.app.loader)
     const dirStack = useSelector(state => state.files.dirStack)
     const [dragEnter, setDragEnter] = useState(false)
+    const [sort, setSort] = useState('type')
 
     useEffect(() => {
-        dispatch(getFiles(currentDir))
-    }, [currentDir])
+        dispatch(getFiles(currentDir, sort))
+    }, [currentDir, sort])
 
     function showPopupHandler() {
         dispatch(setPopupDisplay('flex'))
@@ -51,6 +53,14 @@ const Disk = () => {
         setDragEnter(false)
     }
 
+    if(loader) {
+        return (
+            <div className="loader">
+                <div className="lds-dual-ring"></div>
+            </div>
+        )
+    }
+
     return ( !dragEnter ?
             <div className="disk" onDragEnter={dragEnterHandler} onDragLeave={dragLeaveHandler} onDragOver={dragEnterHandler}>
                 <div className="disk__btns">
@@ -60,6 +70,15 @@ const Disk = () => {
                         <label htmlFor="disk__upload-input" className="disk__upload-label">Загрузить файл</label>
                         <input multiple={true} onChange={(event)=> fileUploadHandler(event)} type="file" id="disk__upload-input" className="disk__upload-input"/>
                     </div>
+                    <select value={sort}
+                            onChange={(e) => setSort(e.target.value)}
+                            className='disk__select'>
+                        <option value="name">По имени</option>
+                        <option value="type">По типу</option>
+                        <option value="date">По дате</option>
+                    </select>
+                    <button className="disk__plate" onClick={() => dispatch(setFileView('plate'))}/>
+                    <button className="disk__list" onClick={() => dispatch(setFileView('list'))}/>
                 </div>
                 <FileList/>
                 <Popup/>
